54747607cd1679071f0c74b769d2940fed0b4bf1
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-07-23 19:11:17 +0100

    creating a function that has a loop to create multiple divs

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,3 +4,17 @@ const grid_con = document.createElement("div");
 grid_con.classList.add("grid-con");
 // adding the grid-con into the html file
 document.body.appendChild(grid_con)
+
+// Functon multiple cells
+function multipleCells(number) {
+    const squareNums = number * number
+//make a loop that will keep making cells 
+    for(let i = 0; i <= squareNums ; i ++) {
+        const newCell = document.createElement("div")
+        grid_con.appendChild(newCell)
+    }
+}
+
+
+// Call mulipleCells
+multipleCells(16);
