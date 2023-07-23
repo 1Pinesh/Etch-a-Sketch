97b639618df59c1177c75ee3dda85280896d93d4
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-07-23 19:04:58 +0100

    giving the contatiner a class name

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,6 @@
 // Making a container
 const grid_con = document.createElement("div");
+// give the grid-con an id
+grid_con.classList.add("grid-con");
 // adding the grid-con into the html file
 document.body.appendChild(grid_con)
