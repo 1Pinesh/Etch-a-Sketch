04e24e7a266f0b17a209494fe2d02e2b007cad3c
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-07-23 19:16:03 +0100

    adding in a container width and height

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,13 @@
 // Making a container
 const grid_con = document.createElement("div");
+
 // give the grid-con an id
 grid_con.classList.add("grid-con");
+
+//giving the container 500 width and height
+grid_con.style.width = "500px";
+grid_con.style.height = "500px";
+
 // adding the grid-con into the html file
 document.body.appendChild(grid_con)
 
